mhagander/hamn

commit baefbd4de052774e75c52e7830ba396b4c8b3664

Save feed status directly instead of at end of processing
Since processing can take a long time with slow blogs, store the
date before that happens.

This will prevent from conflicting with the web UI and overwriting
the settings from that one.

hamnadmin/hamnadmin/util/aggregate.py
@@ -105,11 +105,13 @@ class FeedFetcher(object):
 				d = datetime.datetime.now()
 
 			self.feed.lastget = d
+			self.feed.save()
 		else:
 			# We didn't get a Last-Modified time, so set it to the entry date
 			# for the latest entry in this feed.
 			if self.newest_entry_date:
 				self.feed.lastget = self.newest_entry_date
+				self.feed.save()
 
 	def matches_filter(self, entry):
 		# For now, we only match against self.feed.authorfilter. In the future,

hamnadmin/hamnadmin/register/management/commands/aggregate_feeds.py
@@ -86,8 +86,6 @@ class Command(BaseCommand):
 								entries += 1
 								titles.append(entry.title)
 								total_entries += 1
-						# Save an update to when the feed was last scanned
-						feed.save()
 
 						if entries > 0 and feed.approved:
 							# Email a notification that they were picked up

commit 409bcb3f8fbe228992ba102452315ccc158eae5e

Don't try to store status updates for fake blog entries
We create fake entries when scanning for saved blogs, and they should
never be saved to the db.

This was broken by baefbd4de052774e75c52e7830ba396b4c8b3664 which pushed
the save into the aggregator. Now, make it optional.

hamnadmin/hamnadmin/register/forms.py
@@ -29,7 +29,7 @@ class BlogEditForm(forms.ModelForm):
 
 		# Create a fake instance to pass down. We'll just throw it away
 		feedobj = Blog(feedurl=self.cleaned_data['feedurl'], authorfilter=self.cleaned_data['authorfilter'])
-		fetcher = FeedFetcher(feedobj, _trace)
+		fetcher = FeedFetcher(feedobj, _trace, False)
 		try:
 			entries = list(fetcher.parse())
 		except Exception, e:

hamnadmin/hamnadmin/util/aggregate.py
@@ -7,9 +7,10 @@ import feedparser
 from hamnadmin.register.models import Post
 
 class FeedFetcher(object):
-	def __init__(self, feed, tracefunc=None):
+	def __init__(self, feed, tracefunc=None, update=True):
 		self.feed = feed
 		self.tracefunc = tracefunc
+		self.update = update
 		self.newest_entry_date = None
 
 	def _trace(self, msg):
@@ -104,12 +105,13 @@ class FeedFetcher(object):
 				# currently define rediculously long as 5 days
 				d = datetime.datetime.now()
 
-			self.feed.lastget = d
-			self.feed.save()
+			if self.update:
+				self.feed.lastget = d
+				self.feed.save()
 		else:
 			# We didn't get a Last-Modified time, so set it to the entry date
 			# for the latest entry in this feed.
-			if self.newest_entry_date:
+			if self.newest_entry_date and self.update:
 				self.feed.lastget = self.newest_entry_date
 				self.feed.save()
 