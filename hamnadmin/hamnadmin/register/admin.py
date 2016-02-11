2471326c81476b33430d13feb8e9f69775861d24
Magnus Hagander <magnus@hagander.net>
2016-02-11 14:30:53 +0100

    Add search fields to admin

--- hamnadmin/hamnadmin/register/admin.py
+++ hamnadmin/hamnadmin/register/admin.py
@@ -7,6 +7,7 @@ from hamnadmin.register.models import *
 class BlogAdmin(admin.ModelAdmin):
 	list_display = ['user', 'approved', 'name', 'feedurl', 'authorfilter', ]
 	ordering = ['approved', 'name', ] #meh, multiple ordering not supported
+	search_fields = ['user__username', 'name', 'feedurl']
 
 	def change_view(self, request, object_id, extra_context=None):
 		blog = Blog(pk=object_id)
@@ -17,6 +18,7 @@ class BlogAdmin(admin.ModelAdmin):
 
 class PostAdmin(admin.ModelAdmin):
 	list_display = ['dat', 'title', 'hidden', 'feed']
+	search_fields = ['title', 'feed__name', 'feed__feedurl']
 
 class AggregatorLogAdmin(admin.ModelAdmin):
 	list_display = ['ts', 'success', 'feed', 'info']
