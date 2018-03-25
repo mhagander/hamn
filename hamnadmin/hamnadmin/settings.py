02a8b8dd82f7b24a1051a697ea121f4356a19950
Magnus Hagander <magnus@hagander.net>
2018-03-25 16:56:56 +0200

    Fix template loaders for django 1.11
    Seems django 1.11 automatically enables caching template loader, which
    of course breaks the ability to make any changes to the pages of a
    website without restarting it. And there is no way to turn it off other
    than to explicitly configure individual loders (the logic to turn it on
    in non-debug configurations is hardcoded and cannot be changed).

--- hamnadmin/hamnadmin/settings.py
+++ hamnadmin/hamnadmin/settings.py
@@ -43,12 +43,15 @@ ROOT_URLCONF = 'hamnadmin.urls'
 TEMPLATES = [{
 	'BACKEND': 'django.template.backends.django.DjangoTemplates',
 	'DIRS': [os.path.join(os.path.dirname(__file__), '../../template')],
-	'APP_DIRS': True,
 	'OPTIONS': {
 		'context_processors': [
 			'django.template.context_processors.request',
 			'django.contrib.auth.context_processors.auth',
 			'django.contrib.messages.context_processors.messages',
+		],
+		'loaders': [
+			'django.template.loaders.filesystem.Loader',
+			'django.template.loaders.app_directories.Loader',
 		],
 	},
 }]
