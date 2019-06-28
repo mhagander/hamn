fbdbddf9e1bc71b4080d70f9f77eb4ab1121800d
Magnus Hagander <magnus@hagander.net>
2019-06-28 15:02:16 +0200

    Switch to using requests for varnish calls

--- hamnadmin/hamnadmin/util/varnish.py
+++ hamnadmin/hamnadmin/util/varnish.py
@@ -1,18 +1,17 @@
 from django.conf import settings
 
-import urllib.request, urllib.error, urllib.parse
+import requests
 
 def purge_url(url):
 	if not settings.VARNISH_URL:
 		print("Not purging {0}".format(url))
 	else:
 		try:
-			request = urllib2.Request(settings.VARNISH_URL, headers={
+			r = requests.get(settings.VARNISH_URL, headers={
 				'X-Purge': '^' + url,
-				})
-			response = urllib2.urlopen(request, timeout=2)
-			if response.getcode() != 200:
-				raise Exception("Invalid response code %s" % response.getcode())
+			})
+			if r.status_code != 200:
+				raise Exception("Invalid response code %s" % r.status_code)
 		except Exception as e:
 			raise Exception("Failed to purge '{0}': {1}'".format(url, e))
 
