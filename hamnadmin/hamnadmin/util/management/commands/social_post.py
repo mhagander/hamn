ba4d33814a8695992aaa303e95969e490a8cbfa9
Magnus Hagander <magnus@hagander.net>
2026-04-10 11:08:35 +0200

    Post the title, not the contents
    Oops.

--- hamnadmin/hamnadmin/util/management/commands/social_post.py
+++ hamnadmin/hamnadmin/util/management/commands/social_post.py
@@ -55,7 +55,7 @@ class Command(BaseCommand):
 
             msg = "%s: %s\n\n%s\n\n#postgresql" % (
                 post.feed.name,
-                post.txt,
+                post.title,
                 post.shortlink,
             )
 
